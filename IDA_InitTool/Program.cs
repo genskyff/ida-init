281f035219e3012ba9c1eeea659ee239cc97f525
Kirisame <gensky377@gmail.com>
2022-02-26 15:58:54 +0900

    update text

--- IDA_InitTool/Program.cs
+++ IDA_InitTool/Program.cs
@@ -13,9 +13,9 @@ namespace IDA77_InitTool
         static void Main(string[] args)
         {
             ForegroundColor = Cyan;
-            WriteLine("=======================================");
-            WriteLine("     IDA 7.7 绿化工具 by Binwalker");
-            WriteLine("=======================================\n");
+            WriteLine("==============================================");
+            WriteLine("     IDA Initialization Tool by Binwalker");
+            WriteLine("==============================================\n");
             ResetColor();
 
             var currentPath = Environment.CurrentDirectory;
@@ -29,32 +29,32 @@ namespace IDA77_InitTool
             if (idaCheckFlag == IDAExistFlag.None)
             {
                 ForegroundColor = Red;
-                WriteLine("错误: 请将本程序放到 IDA 根目录");
+                WriteLine("Error: Please move this program to the IDA folder.");
                 ResetColor();
                 goto Exit;
             }
             else if (idaCheckFlag == IDAExistFlag.IDA32)
             {
                 ForegroundColor = Yellow;
-                WriteLine($"警告: 未在 IDA 根目录下找到 <{Path.GetFileName(ida64Path)}>\n");
+                WriteLine($"Warning: Cannot find <{Path.GetFileName(ida64Path)}> in IDA folder.\n");
                 ResetColor();
-                WriteLine("IDA x32 路径: " + idaPath);
+                WriteLine("IDA x32 path: " + idaPath);
             }
             else if (idaCheckFlag == IDAExistFlag.IDA64)
             {
                 ForegroundColor = Yellow;
-                WriteLine($"警告: 未在 IDA 根目录下找到 <{Path.GetFileName(idaPath)}>\n");
+                WriteLine($"Warning: Cannot find <{Path.GetFileName(idaPath)}> in IDA folder.\n");
                 ResetColor();
-                WriteLine("IDA x64 路径: " + ida64Path);
+                WriteLine("IDA x64 path: " + ida64Path);
             }
             else if (idaCheckFlag == (IDAExistFlag.IDA32 | IDAExistFlag.IDA64))
             {
-                WriteLine("IDA x32 路径: " + idaPath);
-                WriteLine("IDA x64 路径: " + ida64Path);
+                WriteLine("IDA x32 path: " + idaPath);
+                WriteLine("IDA x64 path: " + ida64Path);
             }
 
-            WriteLine("\n------------------------------\n");
-            Write("是否一键绿化而不单独设置选项? (y/n) (默认: Yes): ");
+            WriteLine("\n-----------------------------------\n");
+            Write("Initialize without modifying details? (y/n) (Default: Yes): ");
             var cosInput = ReadLine()?.ToLower();
             if (cosInput == null || cosInput.Length == 0 ||
                 cosInput.CompareTo("n") != 0 && cosInput.CompareTo("no") != 0)
@@ -62,14 +62,14 @@ namespace IDA77_InitTool
                 if (pyCheckFlag == PythonExistFlag.None)
                 {
                     ForegroundColor = Red;
-                    WriteLine($"\n错误: 未在 IDA 根目录下找到 <{Path.GetFileName(pyDirPath)}> 目录");
+                    WriteLine($"\nError: Cannot find <{Path.GetFileName(pyDirPath)}/> in IDA folder.");
                     ResetColor();
                     goto Exit;
                 }
                 else if (pyCheckFlag == PythonExistFlag.Dir)
                 {
                     ForegroundColor = Red;
-                    WriteLine($"\n错误: 未在 <python38> 目录下找到 <{Path.GetFileName(pyFilePath)}> 文件");
+                    WriteLine($"\nError: Cannot find <{Path.GetFileName(pyFilePath)}> in <python38/>.");
                     ResetColor();
                     goto Exit;
                 }
@@ -77,24 +77,23 @@ namespace IDA77_InitTool
                 if (!SetIDAReg(pyFilePath))
                 {
                     ForegroundColor = Red;
-                    WriteLine(@"\n错误: 打开注册项 <HKEY_CURRENT_USER\SOFTWARE> 失败");
+                    WriteLine(@"\nError: Open registry key <HKEY_CURRENT_USER\SOFTWARE> failed.");
                     ResetColor();
                     goto Exit;
                 }
                 else
                 {
-                    WriteLine("\n绿化版 Python 路径: " + $"{pyDirPath}");
-                    WriteLine("已禁用 IDA 自动联网");
+                    WriteLine("\nEmbedded python path: " + $"{pyDirPath}");
                 }
 
                 SetIDADesktopShortcut(idaCheckFlag, idaPath, ida64Path);
-                WriteLine("已创建桌面快捷方式");
+                WriteLine("Desktop shortcut created.");
 
                 goto OK;
             }
 
-            WriteLine("\n------------------------------\n");
-            Write("是否使用绿化版 Python? (y/n) (默认: Yes): ");
+            WriteLine("\n-----------------------------------\n");
+            Write("Use the embedded python? (y/n) (Default: Yes): ");
             var pyInput = ReadLine()?.ToLower();
             if (pyInput == null || pyInput.Length == 0 ||
                 pyInput.CompareTo("n") != 0 && pyInput.CompareTo("no") != 0)
@@ -102,14 +101,14 @@ namespace IDA77_InitTool
                 if (pyCheckFlag == PythonExistFlag.None)
                 {
                     ForegroundColor = Red;
-                    WriteLine($"\n错误: 未在 IDA 根目录下找到 <{Path.GetFileName(pyDirPath)}> 目录");
+                    WriteLine($"\nError: Cannot find <{Path.GetFileName(pyDirPath)}/> in IDA folder.");
                     ResetColor();
                     goto Exit;
                 }
                 else if (pyCheckFlag == PythonExistFlag.Dir)
                 {
                     ForegroundColor = Red;
-                    WriteLine($"\n错误: 未在 <python38> 目录下找到 <{Path.GetFileName(pyFilePath)}> 文件");
+                    WriteLine($"\nError: Cannot find <{Path.GetFileName(pyFilePath)}> in <python38/>.");
                     ResetColor();
                     goto Exit;
                 }
@@ -117,34 +116,35 @@ namespace IDA77_InitTool
                 if (!SetIDAReg(pyFilePath))
                 {
                     ForegroundColor = Red;
-                    WriteLine(@"\n错误: 打开注册项 <HKEY_CURRENT_USER\SOFTWARE> 失败");
+                    WriteLine(@"\nError: Open registry key <HKEY_CURRENT_USER\SOFTWARE> failed.");
                     ResetColor();
                     goto Exit;
                 }
                 else
                 {
-                    WriteLine("\n绿化版 Python 路径: " + $"{pyDirPath}");
-                    WriteLine("已禁用 IDA 自动联网");
+                    WriteLine("\nEmbedded python path: " + $"{pyDirPath}");
                 }
             }
 
-            WriteLine("\n------------------------------\n");
-            Write("是否创建桌面快捷方式? (y/n) (默认: Yes): ");
+            WriteLine("\n-----------------------------------\n");
+            Write("Create a desktop shortcut? (y/n) (Default: Yes): ");
             var scInput = ReadLine()?.ToLower();
             if (scInput == null || scInput.Length == 0 ||
                 scInput.CompareTo("n") != 0 && scInput.CompareTo("no") != 0)
             {
                 SetIDADesktopShortcut(idaCheckFlag, idaPath, ida64Path);
-                WriteLine("\n已创建桌面快捷方式");
+                WriteLine("\nDesktop shortcut created.");
             }
 
         OK:
-            WriteLine("\n------------------------------\n");
+            WriteLine("\n-----------------------------------\n");
+            WriteLine("Disable IDA automatic networking.");
+            WriteLine("\n-----------------------------------\n");
             ForegroundColor = Green;
-            WriteLine("绿化完成");
+            WriteLine("Done.");
             ResetColor();
         Exit:
-            WriteLine("\n请按任意键退出");
+            WriteLine("\nPress any key to exit.");
             ReadLine();
         }
     }
